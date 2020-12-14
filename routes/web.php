4267b731c18c9a4a88bed73e693d022e47a3a6b4
moshimoshinana <74526386+moshimoshinana@users.noreply.github.com>
2020-12-14 20:09:58 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,21 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
 });
+
+
+
+
+  
+
+Route::group(['prefix' => 'XXX']), function() {
+    Route::get('Admin\A@bbb')
+}
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('profile\create', 'Admin\ProfileController@add')
+});
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('profile\edit', 'Admin\ProfileController@edit')    
+});
+}
