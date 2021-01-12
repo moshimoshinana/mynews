89c4dfd7a6d309d48f935086301b9e8becdded4d
a <nnkk1176moshi@gmail.com>
2021-01-12 10:24:49 +0000

    first commit

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,20 @@
+
+
+
+<!--{{-- layouts/admin.blade.phpを読み込む --}}-->
+@extends('layouts.profile')-->
+
+
+<!--{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}-->
+@section('title', 'プロフィールの新規作成')-->
+
+<!--{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}-->
+@section('content')-->
+    <div class="container">-->
+        <div class="row">-->
+            <div class="col-md-8 mx-auto">-->
+                <h2>プロフィール新規作成</h2>-->
+            </div>-->
+        </div>-->
+    </div>-->
+@endsection-->
