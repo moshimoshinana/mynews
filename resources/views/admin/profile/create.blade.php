72690b7af9b7c89cb2e65475171b68043fe83796
a <nnkk1176moshi@gmail.com>
2021-01-19 12:20:55 +0000

    20200119

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -10,10 +10,46 @@
 
 <!--{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}-->
 @section('content')
-    <div class="container">
+     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィール新規作成</h2>
+                <h2>ニュース新規作成</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="gender" rows="20">{{ old('gender') }}</textarea>
+                        </div>
+                    </div>
+                                        <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hobby" rows="20">{{ old('hobby') }}</textarea>
+                        </div>
+                    </div>
+                     <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="送信">
+                </form>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('news/create', 'Admin\NewsController@add');
      Route::post('news/create', 'Admin\NewsController@create');
      Route::get('profile/create','Admin\ProfileController@add');
+     Route::post('profile/create', 'Admin\ProfileController@create');
      Route::get('profile/edit','Admin\ProfileController@edit');
+     Route::post('profile/edit','Admin\ProfileController@update');
 });
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
